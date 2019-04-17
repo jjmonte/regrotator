b01fd7048ea3abb82b3a614702125b65ed2f3e38
Victor Perez <vplocked@Victors-MacBook-Pro.local>
2019-04-16 23:11:04 -0400

    Fixed minor issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,6 +59,7 @@ class App extends Component {
         const categoryH = [];
         const categoryM = [];
         const categoryL = [];
+        const categoryA = [];
         const categoryUnsorted = [];
         data.forEach(album => {
           if (album.Rotation_flag === 1){
@@ -72,6 +73,8 @@ class App extends Component {
             categoryM.push(album);
           } else if (album.Category === 'L'){
             categoryL.push(album);
+          } else if (album.Category === 'A'){
+            categoryA.push(album);
           } else { 
             categoryUnsorted.push(album);
           }
@@ -79,6 +82,7 @@ class App extends Component {
     return (
       <div className="page">
       <div className="insert-album">
+        <h1>Add Album:</h1>
         <div className="insert-album__fields">
           <input
             type="text"
@@ -93,38 +97,35 @@ class App extends Component {
 
           <input
             type="text"
-            style={{ width: "100px" }}
             onChange={e => this.setState({Release_date: e.target.value })}
             placeholder="Release date"
           />
           <input
             type="text"
-            style={{ width: "50px" }}
+            style={{ width: "80px" }}
             onChange={e => this.setState({Category: e.target.value })}
             placeholder="Category"
           />
           <input
             type="text"
-            style={{ width: "1000px", height: "20px" }}
             onChange={e => this.setState({Description: e.target.value })}
             placeholder="Description"
           />
           <input
             type="text"
-            style={{ width: "400px" }}
             onChange={e => this.setState({RIYL: e.target.value })}
             placeholder="RIYL"
           />
           <input
             type="text"
-            style={{ width: "10px" }}
+            style={{ width: "80px" }}
             onChange={e => this.setState({Rotation: e.target.value })}
-            placeholder="Rotation"
+            placeholder="In Rotation?"
           />
                     </div>
           <button
             onClick={() =>
-              this.addAlbum(this.state.Artist, this.state.Album_title, this.state.Release_date, this.state.Category, this.state.Description, this.state.Rotation)
+              this.addAlbum(this.state.Album_title, this.state.Artist, this.state.Release_date, this.state.Category, this.state.Description, this.state.Rotation)
             }
           >
             Add album
@@ -171,6 +172,21 @@ class App extends Component {
           {categoryL.length <= 0
             ? " No albums found"
             : categoryL.map(album => (
+                <div className="album" key={album.Album_id}>
+                  <p className="album__descriptor">{album.Album_title}</p>
+                  <p className="album__descriptor">{album.Artist}</p>
+                  <p className="album__descriptor">{album.Rotation_flag}</p>
+                </div>
+              ))}
+          <h3>A</h3>
+          <div className="album-header">
+            <p className="album__descriptor">Album Title</p>
+            <p className="album__descriptor">Artist</p>
+            <p className="album__descriptor">In Rotation?</p>
+          </div>
+          {categoryA.length <= 0
+            ? " No albums found"
+            : categoryA.map(album => (
                 <div className="album" key={album.Album_id}>
                   <p className="album__descriptor">{album.Album_title}</p>
                   <p className="album__descriptor">{album.Artist}</p>
