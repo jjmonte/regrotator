9f8feda45e40f331cae983845d66ad128f3bf8be
Victor Perez <vjperez@mix.wvu.edu>
2019-07-06 18:04:52 -0400

    bjork

--- frontendfull/src/views/Rotation.jsx
+++ frontendfull/src/views/Rotation.jsx
@@ -40,11 +40,57 @@ const AlbumList = styled.ul`
     background-color: ${props => props.theme.bwPrimary};
 
 `;
+//Please don't murder me jade
 const AlbumItem = styled.li`
     font-size: 2.5em;
     margin: 15px 0;
     font-weight: bolder;
     color: ${props => props.theme.bwSecondary};
+    &.björk{
+        text-shadow: 2px 2px 4px #000000;
+        -webkit-animation: rainbow 5s infinite; 
+        -ms-animation: rainbow 5s infinite;
+        animation: rainbow 5s infinite; 
+    }
+    @-webkit-keyframes rainbow{
+        0%{color: orange;}	
+        10%{color: purple;}	
+        20%{color: red;}
+        30%{color: CadetBlue;}
+        40%{color: yellow;}
+        50%{color: coral;}
+        60%{color: green;}
+        70%{color: cyan;}
+        80%{color: DeepPink;}
+        90%{color: DodgerBlue;}
+        100%{color: orange;}
+    }
+    @-ms-keyframes rainbow{
+        0%{color: orange;}	
+        10%{color: purple;}	
+        20%{color: red;}
+        30%{color: CadetBlue;}
+        40%{color: yellow;}
+        50%{color: coral;}
+        60%{color: green;}
+        70%{color: cyan;}
+        80%{color: DeepPink;}
+        90%{color: DodgerBlue;}
+        100%{color: orange;}
+    }
+    @keyframes rainbow{
+        0%{color: orange;}	
+        10%{color: purple;}	
+        20%{color: red;}
+        30%{color: CadetBlue;}
+        40%{color: yellow;}
+        50%{color: coral;}
+        60%{color: green;}
+        70%{color: cyan;}
+        80%{color: DeepPink;}
+        90%{color: DodgerBlue;}
+        100%{color: orange;}
+    }
 `;
 function Rotation(props) {
     const [albums, setAlbums] = useContext(AlbumContext);
@@ -55,13 +101,22 @@ function Rotation(props) {
             <MainWrapper>
                 <FilterToolbar />
                 <AlbumList>
-                    {albums.map(album => (
+                    {albums.map(album => {
+                        if (album.Artist === "Björk"){
+                            return (
+                            <AlbumItem className="björk" key={album.Album_id}>
+                                <Link to={`${album.Artist.replace(/\s+/g, '-').toLowerCase()}/${album.Album_id}-${album.Album_title.replace(/\s+/g, '-').toLowerCase()}/`}>
+                                    {album.Artist.toUpperCase()} - {album.Album_title.toUpperCase()}
+                                </Link>
+                            </AlbumItem>);
+                    } else 
+                        return (
                         <AlbumItem key={album.Album_id}>
                             <Link to={`${album.Artist.replace(/\s+/g, '-').toLowerCase()}/${album.Album_id}-${album.Album_title.replace(/\s+/g, '-').toLowerCase()}/`}>
                                 {album.Artist.toUpperCase()} - {album.Album_title.toUpperCase()}
                             </Link>
-                        </AlbumItem>
-                    ))}
+                        </AlbumItem>);
+                    })}
                 </AlbumList>
             </MainWrapper>
         </React.Fragment>
