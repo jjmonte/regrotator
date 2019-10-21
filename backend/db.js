4cd6ec9da53784300b892834f78b0cda52bacdb2
Victor Perez <vjperez@mix.wvu.edu>
2019-10-21 15:30:54 -0400

    Removed sensitive credentials from project

--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,24 @@
+let cred = require("./dbcredentials");
 let mysql = require("mysql");
 var db;
-
 function connectDB() {
-    if (!db) {
-        db = connection = mysql.createConnection({
-             host: "134.209.10.140",
-             user: "webrequest",
-             password: "JpeKR15jYGS54R5j",
-             database: "rotation"
-        });
+  if (!db) {
+    db = connection = mysql.createConnection({
+      host: "134.209.10.140",
+      user: cred.username,
+      password: cred.password,
+      database: "rotation"
+    });
 
-        connection.connect(function (err) {   
-            if (err) {
-                return console.error("error: " + err.message);      
-            }
-            else {
-                console.log("Connected to the MySQL server."); 
-            }
-        });
-    }
-    return db;  
+    connection.connect(function(err) {
+      if (err) {
+        return console.error("error: " + err.message);
+      } else {
+        console.log("Connected to the MySQL server.");
+      }
+    });
+  }
+  return db;
 }
 
 module.exports = connectDB();
